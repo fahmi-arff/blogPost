3f9214a94a5255abc8d3de679d40f10eb6fcd9b5
Fahmi Arief <fahmi.arff@gmail.com>
2019-06-27 20:31:56 +0700

    update test on "instance type using set n save"

--- test/update_test.js
+++ test/update_test.js
@@ -10,8 +10,16 @@ describe('Updating records', () => {
       .then(() => done());
   });
 
-  it('instance type using set n save', () => {
-
+  it('instance type using set n save', done => {
+    joe.set('name', 'Alex');
+    joe.save()
+      // user find return array
+      .then(() => User.find({}))
+      .then(users => {
+        assert(users.length === 1);
+        assert(users[0].name === 'Alex');
+        done();
+      });
   });
 
 });
