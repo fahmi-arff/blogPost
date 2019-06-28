16c40c83774142725cee6c056ea77950d09254c4
Fahmi Arief <fahmi.arff@gmail.com>
2019-06-28 20:05:57 +0700

    define before each on association test

--- test/association_test.js
+++ test/association_test.js
@@ -4,5 +4,17 @@ const Comment  = require('../src/comment');
 const BlogPost = require('../src/blogPost');
 
 describe('Association', () => {
-  
-})
+  let joe, blogPost, comment;
+
+  beforeEach(done => {
+    joe = new User({ name: 'Joe' });
+    blogPost = new BlogPost({ title: 'Mongod', content: 'content mongod'});
+    comment = new Comment({ content: 'Great post'});
+
+    // embedding the document
+    joe.blogPost.push(blogPost);
+    blogPost.comment.push(comment);
+    // association comment to joe
+    comment.user = joe;
+  });
+});
