bb217a9eb4b82e21ef0f280c071f9ff9e6f4fa72
Fahmi Arief <fahmi.arff@gmail.com>
2019-06-28 07:22:53 +0700

    validation for name required more than 2 character

--- test/validation_test.js
+++ test/validation_test.js
@@ -8,6 +8,15 @@ describe('Validating records', () => {
     const validateResult = user.validateSync();
     // using descructive or mean validate.errors.name.message
     const { message } = validateResult.errors.name;
+
     assert(message === 'Name is required.');
   });
+
+  it('requires a  user\'s name longer than 2 characters', () => {
+    const user = new User({ name: 'Al' });
+    const validateResult = user.validateSync();
+    const { message } = validateResult.errors.name;
+
+    assert(message === 'Name must be longer than 2 characters.');
+  })
 }); 
